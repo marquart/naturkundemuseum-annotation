8a280efd3fe58ae01f833bbcf612825305556c4b
marquart <aron.marquart@gmx.de>
2021-12-18 22:26:45 +0100

    Fix bug in parsing UIMA with accumulator

--- Scripts/ParseUIMAXMI.py
+++ Scripts/ParseUIMAXMI.py
@@ -13,7 +13,7 @@ class OCRCorrection(object):
             self.corrected_string = tag["CorrectedString"]
             self.original_string = text[self.begin:self.end]
     
-                
+
 class Corrector(object):
     def __init__(self, tags, text):
         self.corrections = sorted([OCRCorrection(tag, text) for tag in tags], key=lambda x: x.begin)
@@ -23,32 +23,37 @@ class Corrector(object):
     def __len__(self):
         return len(self.corrections)
     
-    def build_offset(self, text):
-        if not self.corrections: return text
+    def apply(self, text):
+        if not self.corrections:
+            return self.text
         new_text = []
         cursor = 0
+        accu = 0
         for correction in self.corrections:
             if correction.processed: continue
             if correction.to_be_deleted:
-                self.offsets[correction.end] = -1* (correction.end-correction.begin)
+                accu += -1* (correction.end-correction.begin)
+                self.offsets[correction.end] = accu
                 new_text.append(text[cursor:correction.begin])
                 cursor = correction.end
                 
             else:
-                self.offsets[correction.end] = len(correction.corrected_string)-len(correction.original_string)
+                accu += len(correction.corrected_string)-len(correction.original_string)
+                self.offsets[correction.end] = accu
                 new_text.append(text[cursor:correction.begin])
                 new_text.append(correction.corrected_string)
                 cursor = correction.end
                 
             correction.processed = True
-        print(self.offsets)
+            
+        #print(self.offsets)
         self.text = ''.join(new_text)
         return self.text
     
     def offset(self, index):
         cursor = index
-        if self.offsets:
-            while 0 <= cursor < len(self.text):
+        if self.offsets and cursor < len(self.text):
+            while 0 <= cursor:
                 if cursor in self.offsets: return self.offsets[cursor] + index
                 cursor -= 1
         return index
@@ -56,12 +61,12 @@ class Corrector(object):
 
 
 class SemanticEntity(object):
-    def __init__(self, tag, corrector, text):
+    def __init__(self, tag, corrector):
         self.id = int(tag["xmi:id"])
         self.type = tag["SemanticClass"]
         self.begin = corrector.offset(int(tag["begin"]))
         self.end = corrector.offset(int(tag["end"]))
-        self.string = text[self.begin:self.end]
+        self.string = corrector.text[self.begin:self.end]
         self.incoming = []
         self.outgoing = []
     
@@ -71,7 +76,7 @@ class SemanticEntity(object):
 
 
 class SemanticProperty(object):
-    def __init__(self, tag, text, entity_map=None):
+    def __init__(self, tag, entity_map=None):
         self.id = int(tag["xmi:id"])
         self.type = tag["SemanticProperty"]
         self.source_id = int(tag["Governor"])
@@ -92,23 +97,23 @@ class SemanticProperty(object):
         return f"{self.source_id} → {self.type} → {self.target_id}"
 
 def parse(filepath):
-    '''
-        returns (Corrected Text: string, Semantic Entities: list, Semantic Properties: list with Pointers to objects in Entities list)
-    '''
+    '''returns (Corrected Text: string, Semantic Entities: list, Semantic Properties: list with Pointers to objects in Entities list)'''
+    
+    print(f"Parsing {filepath}:")
     with open(filepath, 'r', encoding="utf-8") as f:
         xml = BeautifulSoup(f, "xml")
     
     text = xml.find("cas:Sofa")["sofaString"]
     corrector = Corrector(xml.find_all("custom:OCRCorrection"), text)
-    text = corrector.build_offset(text)
-    print(f"Applied {len(corrector)} OCR-Corrections")
+    text = corrector.apply(text)
+    print(f"    Applied {len(corrector)} OCR-Corrections")
     
-    entities = [SemanticEntity(tag, corrector, text) for tag in xml.find_all("custom:SemanticEntities")]
-    print(f"Parsed {len(entities)} Entities")
+    entities = [SemanticEntity(tag, corrector) for tag in xml.find_all("custom:SemanticEntities")]
+    print(f"    Parsed {len(entities)} Entities")
     
     entity_map = {e.id:e for e in entities}
-    properties = [SemanticProperty(tag, text, entity_map) for tag in xml.find_all("custom:SemanticRelations")]
-    print(f"Parsed {len(properties)} Properties")
+    properties = [SemanticProperty(tag, entity_map) for tag in xml.find_all("custom:SemanticRelations")]
+    print(f"    Parsed {len(properties)} Properties")
     
     return text, entities, properties
 
