d8d7445e5af82f71d79e49160da3ba76356ee6c1
marquart <aron.marquart@gmx.de>
2021-12-18 13:31:02 +0100

    Parsing UIMA-XMI is more robust

--- Scripts/ParseUIMAXMI.py
+++ Scripts/ParseUIMAXMI.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 
 class OCRCorrection(object):
-    def __init__(self, tag):
+    def __init__(self, tag, text):
         self.processed = False
         self.begin = int(tag["begin"])
         self.end = int(tag["end"])
@@ -15,9 +15,13 @@ class OCRCorrection(object):
     
                 
 class Corrector(object):
-    def __init__(self, tags):
-        self.corrections = sorted([OCRCorrection(tag) for tag in tags], key=lambda x: x.begin)
+    def __init__(self, tags, text):
+        self.corrections = sorted([OCRCorrection(tag, text) for tag in tags], key=lambda x: x.begin)
         self.offsets = {}
+        self.text = text
+        
+    def __len__(self):
+        return len(self.corrections)
     
     def build_offset(self, text):
         if not self.corrections: return text
@@ -38,12 +42,13 @@ class Corrector(object):
                 
             correction.processed = True
         print(self.offsets)
-        return ''.join(new_text)
+        self.text = ''.join(new_text)
+        return self.text
     
     def offset(self, index):
         cursor = index
         if self.offsets:
-            while 0 <= cursor < len(text):
+            while 0 <= cursor < len(self.text):
                 if cursor in self.offsets: return self.offsets[cursor] + index
                 cursor -= 1
         return index
@@ -51,7 +56,7 @@ class Corrector(object):
 
 
 class SemanticEntity(object):
-    def __init__(self, tag, corrector):
+    def __init__(self, tag, corrector, text):
         self.id = int(tag["xmi:id"])
         self.type = tag["SemanticClass"]
         self.begin = corrector.offset(int(tag["begin"]))
@@ -66,7 +71,7 @@ class SemanticEntity(object):
 
 
 class SemanticProperty(object):
-    def __init__(self, tag, entity_map=None):
+    def __init__(self, tag, text, entity_map=None):
         self.id = int(tag["xmi:id"])
         self.type = tag["SemanticProperty"]
         self.source_id = int(tag["Governor"])
@@ -86,23 +91,32 @@ class SemanticProperty(object):
         if self.source: return f"{self.source} → {self.type} → {self.target}"
         return f"{self.source_id} → {self.type} → {self.target_id}"
 
+def parse(filepath):
+    '''
+        returns (Corrected Text: string, Semantic Entities: list, Semantic Properties: list with Pointers to objects in Entities list)
+    '''
+    with open(filepath, 'r', encoding="utf-8") as f:
+        xml = BeautifulSoup(f, "xml")
     
+    text = xml.find("cas:Sofa")["sofaString"]
+    corrector = Corrector(xml.find_all("custom:OCRCorrection"), text)
+    text = corrector.build_offset(text)
+    print(f"Applied {len(corrector)} OCR-Corrections")
+    
+    entities = [SemanticEntity(tag, corrector, text) for tag in xml.find_all("custom:SemanticEntities")]
+    print(f"Parsed {len(entities)} Entities")
+    
+    entity_map = {e.id:e for e in entities}
+    properties = [SemanticProperty(tag, text, entity_map) for tag in xml.find_all("custom:SemanticRelations")]
+    print(f"Parsed {len(properties)} Properties")
+    
+    return text, entities, properties
 
 if __name__ == "__main__":
     TEST_FILE = "C:/Users/Aron/Downloads/webanno5013015553902450731export/1887_Museum_67-67.xmi"
     
-    with open(TEST_FILE, 'r', encoding="utf-8") as f:
-        xml = BeautifulSoup(f, "xml")
-    
-    text = xml.find("cas:Sofa")["sofaString"]
-    corrector = Corrector(xml.find_all("custom:OCRCorrection"))
-    text = corrector.build_offset(text)
+    text, entities, properties = parse(TEST_FILE)
     print(text, '\n')
-    
-    entities = [SemanticEntity(tag, corrector) for tag in xml.find_all("custom:SemanticEntities")]
-    entity_map = {e.id:e for e in entities}
-    properties = [SemanticProperty(tag, entity_map) for tag in xml.find_all("custom:SemanticRelations")]
-    
     
     print("\n".join([str(e) for e in properties]))
 
